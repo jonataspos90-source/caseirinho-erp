08eb09f1cfaba989aa5c21707fa0da86d453353a
jonataspos90-source <jonataspos90@gmail.com>
2026-09-06 10:23:54 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="john-erp-pwa-v8-1-20260906";
+const CACHE="john-erp-pwa-v8-1-1-20260906";
 const SHELL=["./","./index.html","./manifest.webmanifest","./offline.html","./icons/icon-192.png","./icons/icon-512.png","./icons/icon-maskable-512.png","./icons/apple-touch-icon.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
